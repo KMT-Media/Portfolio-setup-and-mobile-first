b81604da22dc2d6ca5f32179ba5e15851df4f634
WilhelmK109 <kamulungaw@gmail.com>
2022-11-21 15:47:33 +0200

    add javascript code for mobile popup menu

---
+++ script.js
@@ -0,0 +1,21 @@
+const navMenu = document.querySelector('.nav-menu');
+const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector('.close-menu');
+const listItems = document.querySelectorAll('ul.nav-menu > li');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+listItems.forEach((item) => item.addEventListener('click', close));
+
+function show() {
+  navMenu.style.display = 'flex';
+  navMenu.style.top = '0';
+  closeMenu.style.display = 'block';
+  openMenu.style.display = 'none';
+}
+
+function close() {
+  navMenu.style.top = '-100%';
+  openMenu.style.display = 'block';
+  closeMenu.style.display = 'none';
+}
